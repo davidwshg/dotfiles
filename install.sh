34d666209b5e792c8fa145d7f75c1dcf9224b33d
David Washington <davidwashington833@gmail.com>
2019-03-04 14:55:50 -0300

    Install git, zsh, oh-my-zsh, z and nvm

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ cd; git clone https://github.com/DavidWashington833/dotfiles.git .dotfiles
 mv ~/.bash_profile ~/.dotbackup/.bash_profile && mv ~/.bashrc ~/.dotbackup/.bashrc_backup
 cp ~/.dotfiles/.bash_profile ~/.bash_profile && cp ~/.dotfiles/.bashrc ~/.bashrc_backup
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -O "https://raw.githubusercontent.com/rupa/z/master/z.sh" && mv z.sh ~/.dotfiles/z.sh
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
