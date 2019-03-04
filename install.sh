e1985be74e42b7f7b7477810f4f16e2c02640f13
David Washington <davidwashington833@gmail.com>
2019-03-04 15:24:38 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ apt install git zsh -y
 cd; git clone https://github.com/DavidWashington833/dotfiles.git ~/.dotfiles
 
 mkdir ~/.dotbackup
-mv ~/.bash_profile ~/.dotbackup/.bash_profile
-mv ~/.bashrc ~/.dotbackup/.bashrc_backup
+mv ~/.bashrc ~/.dotbackup/
+mv ~/.bash_profile ~/.dotbackup/
+cp ~/.dotfiles/.bashrc ~/.bashrc
 cp ~/.dotfiles/.bash_profile ~/.bash_profile
-cp ~/.dotfiles/.bashrc ~/.bashrc_backup
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -O "https://raw.githubusercontent.com/rupa/z/master/z.sh" && mv z.sh ~/.dotfiles/z.sh
