725ad3306228a6f96c8cbc7fcc6d41962db4caf8
David Washington <davidwashington833@gmail.com>
2019-03-04 15:13:23 -0300

    Add .gitignore and update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 apt update -y
 apt install git zsh -y
 
-cd; git clone https://github.com/DavidWashington833/dotfiles.git .dotfiles
+cd; git clone https://github.com/DavidWashington833/dotfiles.git ~/.dotfiles
 
-mv ~/.bash_profile ~/.dotbackup/.bash_profile && mv ~/.bashrc ~/.dotbackup/.bashrc_backup
-cp ~/.dotfiles/.bash_profile ~/.bash_profile && cp ~/.dotfiles/.bashrc ~/.bashrc_backup
+mv ~/.bash_profile ~/.dotbackup/.bash_profile
+mv ~/.bashrc ~/.dotbackup/.bashrc_backup
+cp ~/.dotfiles/.bash_profile ~/.bash_profile
+cp ~/.dotfiles/.bashrc ~/.bashrc_backup
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -O "https://raw.githubusercontent.com/rupa/z/master/z.sh" && mv z.sh ~/.dotfiles/z.sh
