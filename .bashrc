1ce4039526a91f7ebae76863d3726490bc60a7c5
David Washington <davidwashington833@gmail.com>
2019-03-04 14:54:39 -0300

    Install git, zsh, oh-my-zsh, z and nvm

---
+++ .bashrc
@@ -0,0 +1,4 @@
+export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+~/.dotfiles/z.sh
---
+++ install.sh
@@ -0,0 +1,10 @@
+apt update -y
+apt install git zsh -y
+
+cd; git clone https://github.com/DavidWashington833/dotfiles.git .dotfiles
+
+mv ~/.bash_profile ~/.dotbackup/.bash_profile && mv ~/.bashrc ~/.dotbackup/.bashrc_backup
+cp ~/.dotfiles/.bash_profile ~/.bash_profile && cp ~/.dotfiles/.bashrc ~/.bashrc_backup
+
+curl -O "https://raw.githubusercontent.com/rupa/z/master/z.sh" && mv z.sh ~/.dotfiles/z.sh
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
