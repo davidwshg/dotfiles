2e44fbe73e6f09683a36996617bc01c6f0130f92
davidwshg <davidwshg@gmail.com>
2023-05-15 23:31:49 -0300

    refactor: remove unnecessary configs and .zprofile file

--- config.sh
+++ config.sh
@@ -4,6 +4,4 @@ git clone https://github.com/davidwfs/dotfiles.git ~/.dotfiles
 
 mkdir ~/.dotbackup
 mv ~/.zshrc ~/.dotbackup/
-mv ~/.zprofile ~/.dotbackup/
 cp ~/.dotfiles/.zshrc ~/
-cp ~/.dotfiles/.zprofile ~/
